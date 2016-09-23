0ffb3e8afaab54aa2100feaed8719a555ba21c77
dgandivalasa <Divya_Gandivalasa@epam.com>
2016-09-23 17:48:42 +0530

    scriptcs solution

---
+++ src/ScriptCs.Contracts/IScriptEnvironment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.Contracts
+{
+    public interface IScriptEnvironment
+    {
+        IReadOnlyList<string> ScriptArgs { get; }
+        void AddCustomPrinter<T>(Func<T, string> printer);
+        void Print<T>(T o);
+        void Print(object o);
+    }
+}
---
+++ src/ScriptCs.Core/ScriptHostFactory.cs
@@ -0,0 +1,21 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs
+{
+    public class ScriptHostFactory : IScriptHostFactory
+    {
+        private readonly IConsole _console;
+        private readonly Printers _printers;
+
+        public ScriptHostFactory(IConsole console, Printers printers)
+        {
+            _console = console;
+            _printers = printers;
+        }
+
+        public IScriptHost CreateScriptHost(IScriptPackManager scriptPackManager, string[] scriptArgs)
+        {
+            return new ScriptHost(scriptPackManager, new ScriptEnvironment(scriptArgs, _console, _printers));
+        }
+    }
+}
